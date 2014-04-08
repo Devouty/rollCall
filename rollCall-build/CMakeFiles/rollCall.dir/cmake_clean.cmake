901faebf327c5e8ba1be5d665f81146037996d25
Devouty <devoutyquan@gmail.com>
2014-04-08 21:47:24 +0800

    转换成Ubuntu13.10系统，为了自学，会与Windows7旗舰版同时编写

---
+++ rollCall-build/CMakeFiles/rollCall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rollCall.dir/main.cpp.o"
+  "rollCall.pdb"
+  "rollCall"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rollCall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ rollCall/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "srhrhj,mnb" << endl;
+    return 0;
+}
+
